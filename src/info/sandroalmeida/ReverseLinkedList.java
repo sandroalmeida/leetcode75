ef9cd32031cc0e13ad44fa128f99f5d8ca6422f2
sandroalmeida <sandro.almeida@dotbuild.com.br>
2021-10-06 14:44:19 -0700

    ReverseLinkedList implementation.

---
+++ src/info/sandroalmeida/ReverseLinkedList.java
@@ -0,0 +1,40 @@
+package info.sandroalmeida;
+
+public class ReverseLinkedList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(){}
+        ListNode(int val){
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val; this.next = next;
+        }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode current = head;
+        ListNode previous = null;
+
+        while(current != null){
+            ListNode temp = current.next;
+            current.next = previous;
+            previous = current;
+            current = temp;
+        }
+        return previous;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        ListNode result = reverseList(head);
+    }
+}
