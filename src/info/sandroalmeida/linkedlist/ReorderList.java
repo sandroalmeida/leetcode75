8298dd0ff7a356b961701c9f6d3d3b25345562ba
sandroalmeida <sandro.almeida@dotbuild.com.br>
2021-10-07 11:17:25 -0700

    ReorderList implementation.

---
+++ src/info/sandroalmeida/linkedlist/ReorderList.java
@@ -0,0 +1,88 @@
+package info.sandroalmeida.linkedlist;
+
+/**
+ * 143. Reorder List
+ * https://leetcode.com/problems/reorder-list/
+ *
+ * You are given the head of a singly linked-list. The list can be represented as:
+ *
+ * L0 → L1 → … → Ln - 1 → Ln
+ * Reorder the list to be on the following form:
+ * L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+ * You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+ *
+ * Example 1:
+ * Input: head = [1,2,3,4]
+ * Output: [1,4,2,3]
+ *
+ * Example 2:
+ * Input: head = [1,2,3,4,5]
+ * Output: [1,5,2,4,3]
+ *
+ * Constraints:
+ * The number of nodes in the list is in the range [1, 5 * 104].
+ * 1 <= Node.val <= 1000
+ * */
+public class ReorderList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(){}
+        ListNode(int val){
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val; this.next = next;
+        }
+    }
+
+    public static void reorderList(ListNode head) {
+        if(head==null||head.next==null) return;
+
+        //Find the middle of the list
+        ListNode p1=head;
+        ListNode p2=head;
+        while(p2.next!=null&&p2.next.next!=null){
+            p1=p1.next;
+            p2=p2.next.next;
+        }
+
+        //Reverse the half after middle  1->2->3->4->5->6 to 1->2->3->6->5->4
+        ListNode preMiddle=p1;
+        ListNode preCurrent=p1.next;
+        while(preCurrent.next!=null){
+            ListNode current=preCurrent.next;
+            preCurrent.next=current.next;
+            current.next=preMiddle.next;
+            preMiddle.next=current;
+        }
+
+        //Start reorder one by one  1->2->3->6->5->4 to 1->6->2->5->3->4
+        p1=head;
+        p2=preMiddle.next;
+        while(p1!=preMiddle){
+            preMiddle.next=p2.next;
+            p2.next=p1.next;
+            p1.next=p2;
+            p1=p2.next;
+            p2=preMiddle.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(4);
+        listNode.next.next.next.next = new ListNode(5);
+        listNode.next.next.next.next.next = new ListNode(6);
+
+        reorderList(listNode);
+        while(listNode != null){
+            System.out.print(listNode.val + "-");
+            listNode = listNode.next;
+        }
+    }
+}
