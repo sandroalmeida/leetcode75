4cf7505e452322bab97309cc326b10b284b618d3
sandroalmeida <sandro.almeida@dotbuild.com.br>
2021-10-08 13:03:52 -0700

    BinaryTreeMaximumPathSum_V2 implementation.

---
+++ src/info/sandroalmeida/tree/BinaryTreeMaximumPathSum_V2.java
@@ -0,0 +1,80 @@
+package info.sandroalmeida.tree;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ * 124. Binary Tree Maximum Path Sum
+ * https://leetcode.com/problems/binary-tree-maximum-path-sum/
+ * MY FIRST SOLUTION I DIDN'T CONSIDER A PARENT NODE AS POSSIBLE PATH
+ * EXAMPLE A PARENT NODE 2 WITH LEFT NODE -1, MAX_SUM_PATH FOR LEETCODE IS 2, JUST PARENT VALUE.
+ *
+ * A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them.
+ * A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+ * The path sum of a path is the sum of the node's values in the path.
+ * Given the root of a binary tree, return the maximum path sum of any path.
+ *
+ * Example 1:
+ * Input: root = [1,2,3]
+ * Output: 6
+ * Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
+ *
+ * Example 2:
+ * Input: root = [-10,9,20,null,null,15,7]
+ * Output: 42
+ * Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
+ *
+ * Constraints:
+ * The number of nodes in the tree is in the range [1, 3 * 104].
+ * -1000 <= Node.val <= 1000
+ * */
+public class BinaryTreeMaximumPathSum_V2 {
+
+    static int maxValue;
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static int maxPathSum(TreeNode root) {
+        maxValue = Integer.MIN_VALUE;
+        helper(root);
+        return maxValue;
+    }
+
+    public static int helper(TreeNode node) {
+
+        if (node == null) return 0;
+        int left = Math.max(0, helper(node.left));
+        int right = Math.max(0, helper(node.right));
+        maxValue = Math.max(maxValue, left + right + node.val);
+        return Math.max(left, right) + node.val;
+
+    }
+
+    public static void main(String[] args) {
+        TreeNode tree = new TreeNode(10);
+        tree.left = new TreeNode(8);
+        tree.left.left = new TreeNode(6);
+        tree.left.right = new TreeNode(9);
+        tree.right = new TreeNode(12);
+        tree.right.left = new TreeNode(11);
+        tree.right.right = new TreeNode(14);
+
+        System.out.println(maxPathSum(tree));
+    }
+}
