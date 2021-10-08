995878738ab15d48c377d6302fbd420c474a9f3e
sandroalmeida <sandro.almeida@dotbuild.com.br>
2021-10-08 13:31:03 -0700

    BinaryTreeLevelOrderTraversal implementation.

---
+++ src/info/sandroalmeida/tree/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,79 @@
+package info.sandroalmeida.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 102. Binary Tree Level Order Traversal
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/
+ *
+ * Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+ *
+ * Example 1:
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: [[3],[9,20],[15,7]]
+ *
+ * Example 2:
+ * Input: root = [1]
+ * Output: [[1]]
+ *
+ * Example 3:
+ * Input: root = []
+ * Output: []
+ *
+ * Constraints:
+ * The number of nodes in the tree is in the range [0, 2000].
+ * -1000 <= Node.val <= 1000
+ * */
+public class BinaryTreeLevelOrderTraversal {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null)
+            return result;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+            for(int i = 0; i < levelSize; i++){
+                TreeNode currentNode = queue.poll(); 
+                currentLevel.add(currentNode.val);
+                if(currentNode.left != null)
+                    queue.add(currentNode.left);
+                if(currentNode.right != null)
+                    queue.add(currentNode.right);
+            }
+            result.add(currentLevel);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(3);
+        node.left = new TreeNode(9);
+        node.right = new TreeNode(20);
+        node.right.left = new TreeNode(15);
+        node.right.right = new TreeNode(7);
+        List<List<Integer>> result = levelOrder(node);
+    }
+}
