06651e98fea326ace35aafe9229c7d7d4c47c521
sandroalmeida <sandro.almeida@dotbuild.com.br>
2021-10-09 10:39:49 -0700

    ConstructBinaryTreePreorderInorder implementation.

---
+++ src/info/sandroalmeida/tree/ConstructBinaryTreePreorderInorder.java
@@ -0,0 +1,69 @@
+package info.sandroalmeida.tree;
+
+import java.util.Arrays;
+
+/**
+ * 105. Construct Binary Tree from Preorder and Inorder Traversal
+ * https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+ *
+ * Given two integer arrays preorder and inorder where preorder is the preorder traversal of a
+ * binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+ *
+ * Example 1:
+ * Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+ * Output: [3,9,20,null,null,15,7]
+ *
+ * Example 2:
+ * Input: preorder = [-1], inorder = [-1]
+ * Output: [-1]
+ *
+ * Constraints:
+ *
+ * 1 <= preorder.length <= 3000
+ * inorder.length == preorder.length
+ * -3000 <= preorder[i], inorder[i] <= 3000
+ * preorder and inorder consist of unique values.
+ * Each value of inorder also appears in preorder.
+ * preorder is guaranteed to be the preorder traversal of the tree.
+ * inorder is guaranteed to be the inorder traversal of the tree.
+ * */
+public class ConstructBinaryTreePreorderInorder {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        if(preorder.length == 0 || inorder.length ==  0)
+            return null;
+        TreeNode root = new TreeNode(preorder[0]);
+        int mid;
+        for(mid = 0; mid < inorder.length; mid++){
+            if(inorder[mid] == preorder[0]) break;
+        }
+        int[] leftPreorder = Arrays.copyOfRange(preorder, 1, mid + 1);
+        int[] leftInorder = Arrays.copyOfRange(inorder, 0, mid);
+        root.left = buildTree(leftPreorder, leftInorder);
+
+        int[] rightPreorder = Arrays.copyOfRange(preorder, mid + 1, preorder.length);
+        int[] rightInorder = Arrays.copyOfRange(inorder, mid + 1, inorder.length);
+        root.right = buildTree(rightPreorder, rightInorder);
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int[] preorder = new int[]{3,9,20,15,7}, inorder = new int[]{9,3,15,20,7};
+        TreeNode root = buildTree(preorder, inorder);
+    }
+}
