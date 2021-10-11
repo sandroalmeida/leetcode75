cead73c3b895211cc6bb7768f5c1236f8c78c9c5
sandroalmeida <sandro.almeida@dotbuild.com.br>
2021-10-11 13:27:33 -0700

    SetMatrixZeroes_V2 implementation.

---
+++ src/info/sandroalmeida/matrix/SetMatrixZeroes_V2.java
@@ -0,0 +1,63 @@
+package info.sandroalmeida.matrix;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 73. Set Matrix Zeroes
+ * https://leetcode.com/problems/set-matrix-zeroes/
+ *
+ * Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's, and return the matrix.
+ *
+ * You must do it in place.
+ *
+ * Example 1:
+ * Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+ * Output: [[1,0,1],[0,0,0],[1,0,1]]
+ *
+ * Example 2:
+ * Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+ * Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ *
+ * Constraints:
+ * m == matrix.length
+ * n == matrix[0].length
+ * 1 <= m, n <= 200
+ * -231 <= matrix[i][j] <= 231 - 1
+ *
+ * Follow up:
+ * A straightforward solution using O(mn) space is probably a bad idea.
+ * A simple improvement uses O(m + n) space, but still not the best solution.
+ * Could you devise a constant space solution?
+ * */
+public class SetMatrixZeroes_V2 {
+
+    public static void setZeroes(int[][] matrix) {
+        int col0 = 1, rows = matrix.length - 1, cols = matrix[0].length - 1;
+
+        for (int i = 0; i <= rows; i++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j = 1; j <= cols; j++)
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
+        }
+
+        for (int i = rows; i >= 0; i--) {
+            for (int j = cols; j >= 1; j--)
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            if (col0 == 0) matrix[i][0] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1,1,1,1,1},{1,0,1,0,1},{1,1,1,1,1}};
+        setZeroes(matrix);
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println("");
+        }
+    }
+}
