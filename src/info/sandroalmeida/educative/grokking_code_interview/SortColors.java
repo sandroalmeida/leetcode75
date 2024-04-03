3f9bd4609cc7bdc364a7bf58946a21d8d7473090
Sandro Almeida <sandrodotbuild@gmail.com>
2024-04-02 21:50:44 -0700

    Sort Colors

---
+++ src/info/sandroalmeida/educative/grokking_code_interview/SortColors.java
@@ -0,0 +1,37 @@
+package info.sandroalmeida.educative.grokking_code_interview;
+
+public class SortColors {
+    public static int[] sortColors (int[] colors) {
+
+        int current = 0;
+        int start = 0;
+        int end = colors.length - 1;
+
+        while(current <= end) {
+            int value = colors[current];
+            if(value == 0) {
+                swap(current, start, colors);
+                current++;
+                start++;
+            } else if(value == 1) {
+                current++;
+            } else {
+                swap(end, current, colors);
+                end--;
+            }
+        }
+        return colors;
+    }
+
+    private static void swap(int idx1, int idx2, int[] array) {
+        int temp = array[idx1];
+        array[idx1] = array[idx2];
+        array[idx2] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] colors = {1,1,0,2,0,2,1,1,2};
+        sortColors(colors);
+        System.out.println("end test");
+    }
+}
