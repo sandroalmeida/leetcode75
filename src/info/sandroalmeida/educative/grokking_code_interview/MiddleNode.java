6b027717e2985d82d1e67b8a302227dac56338d1
Sandro Almeida <sandrodotbuild@gmail.com>
2024-04-03 17:36:22 -0700

    Middle Node

---
+++ src/info/sandroalmeida/educative/grokking_code_interview/MiddleNode.java
@@ -0,0 +1,25 @@
+package info.sandroalmeida.educative.grokking_code_interview;
+
+public class MiddleNode {
+    public static LinkedListNode middleNode(LinkedListNode head) {
+
+        LinkedListNode slow = head;
+        LinkedListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        LinkedListNode head = new LinkedListNode(1);
+        head.next = new LinkedListNode(2);
+        head.next.next = new LinkedListNode(3);
+        head.next.next.next = new LinkedListNode(4);
+        head.next.next.next.next = new LinkedListNode(5);
+        head.next.next.next.next.next = new LinkedListNode(6);
+
+        System.out.println(middleNode(head).data);
+    }
+}
