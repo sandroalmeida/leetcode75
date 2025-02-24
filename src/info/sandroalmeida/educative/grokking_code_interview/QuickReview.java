04b67a4103c38912a87ee3a06f123b17b7d172cf
Sandro Almeida <sandrodotbuild@gmail.com>
2025-02-24 11:31:11 -0700

    quick review

--- src/info/sandroalmeida/educative/grokking_code_interview/QuickReview.java
+++ src/info/sandroalmeida/educative/grokking_code_interview/QuickReview.java
@@ -13,8 +13,8 @@ public class QuickReview {
 
     int[] nums1 = {1,2,3};
     int[] nums2 = {4,5,6};
-    System.out.println(returnMedian(nums1, nums2));
-
+//    System.out.println(returnMedian(nums1, nums2));
+    System.out.println(findMedianSortedArrays(nums1, nums2));
   }
 
   public static int[] twoSum(int[] nums, int target) {
@@ -81,5 +81,42 @@ public class QuickReview {
     } else {
       return merged.get(mIdx);
     }
+  }
+
+  public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    // nums1 will be the smaller array
+    if (nums1.length > nums2.length) {
+      return findMedianSortedArrays(nums2, nums1);
+    }
+
+    int m = nums1.length, n = nums2.length;
+    int low = 0, high = m;
+    while (low <= high) {
+      int partitionX = (low + high) / 2; // Partition index for nums1
+      int partitionY = (m + n + 1) / 2 - partitionX; // Partition index for nums2
+
+      // Handling edge cases where partition is at start or end
+      int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+      int minRightX = (partitionX == m) ? Integer.MAX_VALUE : nums1[partitionX];
+
+      int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+      int minRightY = (partitionY == n) ? Integer.MAX_VALUE : nums2[partitionY];
+
+      if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+        // Found the correct partition
+        if ((m + n) % 2 == 0) {
+          return (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2.0;
+        } else {
+          return Math.max(maxLeftX, maxLeftY);
+        }
+      } else if (maxLeftX > minRightY) {
+        // Too far right in nums1, move left
+        high = partitionX - 1;
+      } else {
+        // Too far left in nums1, move right
+        low = partitionX + 1;
+      }
+    }
+    throw new IllegalArgumentException("Input arrays are not sorted properly.");
   }
 }
